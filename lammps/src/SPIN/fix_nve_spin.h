bfd693ab4b3aa3510b25167b090eb83477bca153
MagTheoryLab <bxu@gscaep.ac.cn>
2026-05-09 17:04:32 +0800

    Use SWD no-langevin predictor in group_sum path

--- lammps/src/SPIN/fix_nve_spin.h
+++ lammps/src/SPIN/fix_nve_spin.h
@@ -35,16 +35,17 @@ class FixNVESpin : public Fix {
   void initial_integrate(int) override;
   void final_integrate() override;
 
-  void ComputeInteractionsSpin(int);    // compute and advance single spin functions
+  void ComputeInteractionsSpin(int, double *);
+  void ComputeInteractionsSpinOneSide(int, double *);
   void AdvanceSingleSpin(int);
+  void AdvanceSingleSpinNoLangevin(int, const double *, double *);
+  void AdvanceSingleSpinPredict(int, const double *, double *);
 
   void sectoring();    // sectoring operation functions
   int coords2sector(double *);
 
   void setup_pre_neighbor() override;
   void pre_neighbor() override;
-  void backup_lattice_force();
-  void restore_lattice_force();
 
   int lattice_flag;    // lattice_flag = 0 if spins only
                        // lattice_flag = 1 if spin-lattice calc.
@@ -94,8 +95,21 @@ class FixNVESpin : public Fix {
   // optional bridge path for NEP spin pair styles without PairSpin interface
   int nep_global_recompute_flag;
   double hbar_local;
-  int nmax_backup;
-  double **f_backup;
+  typedef void (*nep_spin_single_fn_t)(class Pair *, int, double *);
+  nep_spin_single_fn_t nep_compute_single_pair;
+  nep_spin_single_fn_t nep_compute_single_pair_one_side;
+
+  // optional energy-tracking instrumentation
+  int energy_track_flag;
+  int energy_track_every;
+  int iface_axis;
+  double iface_pos;
+  double iface_half_width;
+  int iface_enable;
+  char *energy_group_file;
+  char *energy_iface_file;
+  double *group_sums_local;
+  double *group_sums_global;
 
   // stacking variables for sectoring algorithm
 
